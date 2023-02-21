0a1a4329dff6826a768676178c106569d2cba620
barrymendy <barrymendy@gmail.com>
2023-02-21 11:31:08 +0100

    commit gestion membre

--- admin/gestion_membre.php
+++ admin/gestion_membre.php
@@ -6,6 +6,85 @@ if (!internauteConnecteAdmin()) {
     exit();
 }
 
+// au préalable, pour introduire le formulaire, je vérifie que j'ai reçu dans l'URL un indice action. Ca permettra de ne pas répéter plusieurs fois cette vérification dans tout le traitement du formulaire qui va suivre
+if (isset($_GET['action'])) {
+    if ($_POST) {
+
+        if (!isset($_POST['pseudo']) || !preg_match('#^[a-zA-Z0-9-_.]{3,20}$#', $_POST['pseudo'])) {
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur format pseudo !</div>';
+        }
+
+        if (!isset($_POST['mdp']) || strlen($_POST['mdp']) < 3 || strlen($_POST['mdp']) > 20) {
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur format mdp !</div>';
+        }
+
+        if (!isset($_POST['nom']) || iconv_strlen($_POST['nom']) < 3 || iconv_strlen($_POST['nom']) > 20) {
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur format nom !</div>';
+        }
+
+        if (!isset($_POST['prenom']) || iconv_strlen($_POST['prenom']) < 3 || iconv_strlen($_POST['prenom']) > 20) {
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur format prénom !</div>';
+        }
+
+        if (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur format email !</div>';
+        }
+
+        if (!isset($_POST['civilite']) || $_POST['civilite'] != 'femme' && $_POST['civilite'] != 'homme') {
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur format civilité !</div>';
+        }
+
+        if (!isset($_POST['ville']) || strlen($_POST['ville']) < 2 || strlen($_POST['ville']) > 30) {
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur format ville !</div>';
+        }
+
+        if (!isset($_POST['code_postal']) || !preg_match('#^[0-9]{5}$#', $_POST['code_postal'])) {
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur format code postal !</div>';
+        }
+
+        if (!isset($_POST['adresse']) || strlen($_POST['adresse']) < 5 || strlen($_POST['adresse']) > 50) {
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur format adresse !</div>';
+        }
+
+        $verifPseudo = $pdo->prepare("SELECT pseudo FROM membre WHERE pseudo = :pseudo ");
+        $verifPseudo->bindValue(':pseudo', $_POST['pseudo'], PDO::PARAM_STR);
+        $verifPseudo->execute();
+
+        if ($verifPseudo->rowCount() == 1) {
+
+            $erreur .= '<div class="alert alert-danger" role="alert">Erreur, ce pseudo existe déjà, vous devez en choisir un autre !</div>';
+        }
+
+        $_POST['mdp'] = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
+
+        if (empty($erreur)) {
+            // si dans l'URL action == update, on entame une procédure de modification
+            if($_GET['action'] == 'update'){
+
+            }else{
+                // si on récupère autre chose que update (et donc add) on entame une procédure d'insertion en BDD
+                $inscrireUser = $pdo->prepare(" INSERT INTO membre (pseudo, mdp, nom, prenom, email, civilite, ville, code_postal, adresse) VALUES (:pseudo, :mdp, :nom, :prenom, :email, :civilite, :ville, :code_postal, :adresse) ");
+                $inscrireUser->bindValue(':pseudo', $_POST['pseudo'], PDO::PARAM_STR);
+                $inscrireUser->bindValue(':mdp', $_POST['mdp'], PDO::PARAM_STR);
+                $inscrireUser->bindValue(':nom', $_POST['nom'], PDO::PARAM_STR);
+                $inscrireUser->bindValue(':prenom', $_POST['prenom'], PDO::PARAM_STR);
+                $inscrireUser->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+                $inscrireUser->bindValue(':civilite', $_POST['civilite'], PDO::PARAM_STR);
+                $inscrireUser->bindValue(':ville', $_POST['ville'], PDO::PARAM_STR);
+                $inscrireUser->bindValue(':code_postal', $_POST['code_postal'], PDO::PARAM_INT);
+                $inscrireUser->bindValue(':adresse', $_POST['adresse'], PDO::PARAM_STR);
+                $inscrireUser->execute();
+            }
+        }
+    }
+    // Procedure de recuperation des in infos en BDD pour les afficher dans le formulaire lorsque on fait un update(plus pratique et plus sur)
+    if($_GET['action'] == 'update'){
+        $tousUsers = $pdo->query("SELECT * FROM membre WHERE id_membre = '$_GET[id_membre']");
+        $userActuel = $tousUsers->fetch(PDO::FETCH_ASSOC);
+        $pseudo = (isset($userActuel['pseudo'])) ? $userActuel['pseudo'] : "";
+    }
+}
+
 require_once('includeAdmin/header.php');
 ?>
 
@@ -22,6 +101,8 @@ require_once('includeAdmin/header.php');
     <div class="badge badge-warning text-wrap p-3">Gestion des utilisateurs</div>
 </h1>
 
+<?= $erreur ?>
+
 <!-- <div class="blockquote alert alert-dismissible fade show mt-5 shadow border border-warning rounded" role="alert">
     <p>Gérez ici votre base de données des utilisateurs</p>
     <p>Vous pouvez modifier leurs données, ajouter ou supprimer un utilisateur</p>
@@ -31,103 +112,106 @@ require_once('includeAdmin/header.php');
 </div> -->
 
 <!-- le formulaire et son h2 n'apparaissent que si on en a fait la demande a partir du tableau des données. Sinon, c'est le tableau uniquement qui s'affiche, plus pratique pour l'admin -->
-
-<h2 class="my-5">Formulaire des utilisateurs</h2>
-
-<?php if(isset($_GET['action'])): ?>
-<form class="my-5" method="POST" action="">
+<?php if (isset($_GET['action'])) : ?>
+    <h2 class="my-5">Formulaire <?= ($_GET['action'] == 'add') ? "d'ajout" : "de modification" ?> des utilisateurs</h2>
 
 
+    <form class="my-5" method="POST" action="">
 
-    <div class="row">
-        <div class="col-md-4 mt-5">
-            <label class="form-label" for="pseudo">
-                <div class="badge badge-dark text-wrap">Pseudo</div>
-            </label>
-            <input class="form-control" type="text" name="pseudo" id="pseudo" placeholder="Pseudo">
+        <!-- input pour l'ID_membre, indispensable pour la requete de modification en BDD (on s'en fiche pour l'insertion en BDD). Sans, on ne pourra pas cibler dans la requete sql un user parmi les autres. Par contre, il doit  -->
+        <input type="hidden" name="id_membre" value="">
+
+        <div class="row">
+            <div class="col-md-4 mt-5">
+                <label class="form-label" for="pseudo">
+                    <div class="badge badge-dark text-wrap">Pseudo</div>
+                </label>
+                <input class="form-control" type="text" name="pseudo" id="pseudo" placeholder="Pseudo">
+            </div>
+
+
+
+            <div class="col-md-4 mt-5">
+                <label class="form-label" for="mdp">
+                    <div class="badge badge-dark text-wrap">Mot de passe</div>
+                </label>
+                <input class="form-control" type="password" name="mdp" id="mdp" placeholder="Mot de passe">
+            </div>
+
+
+
+            <div class="col-md-4 mt-5">
+                <label class="form-label" for="email">
+                    <div class="badge badge-dark text-wrap">Email</div>
+                </label>
+                <input class="form-control" type="email" name="email" id="email" placeholder="Email">
+            </div>
         </div>
 
+        <div class="row">
+            <div class="col-md-4 mt-5">
+                <label class="form-label" for="nom">
+                    <div class="badge badge-dark text-wrap">Nom</div>
+                </label>
+                <input class="form-control" type="text" name="nom" id="nom" placeholder="Nom">
+            </div>
 
+            <div class="col-md-4 mt-5">
+                <label class="form-label" for="prenom">
+                    <div class="badge badge-dark text-wrap">Prénom</div>
+                </label>
+                <input class="form-control" type="text" name="prenom" id="prenom" placeholder="Prénom">
+            </div>
 
-        <div class="col-md-4 mt-5">
-            <label class="form-label" for="mdp">
-                <div class="badge badge-dark text-wrap">Mot de passe</div>
-            </label>
-            <input class="form-control" type="password" name="mdp" id="mdp" placeholder="Mot de passe">
+            <div class="col-md-4 mt-4">
+                <p>
+                <div class="badge badge-dark text-wrap">Civilité</div>
+                </p>
+
+                <input type="radio" name="civilite" id="civilite1" value="femme">
+                <label class="mx-2" for="civilite1">Femme</label>
+
+                <input type="radio" name="civilite" id="civilite2" value="homme">
+                <label class="mx-2" for="civilite2">Homme</label>
+            </div>
         </div>
 
+        <div class="row">
+            <div class="col-md-4 mt-5">
+                <label class="form-label" for="ville">
+                    <div class="badge badge-dark text-wrap">Ville</div>
+                </label>
+                <input class="form-control" type="text" name="ville" id="ville" placeholder="Ville">
+            </div>
 
+            <div class="col-md-4 mt-5">
+                <label class="form-label" for="code_postal">
+                    <div class="badge badge-dark text-wrap">Code Postal</div>
+                </label>
+                <input class="form-control" type="text" name="code_postal" id="code_postal" placeholder="Code postal">
+            </div>
 
-        <div class="col-md-4 mt-5">
-            <label class="form-label" for="email">
-                <div class="badge badge-dark text-wrap">Email</div>
-            </label>
-            <input class="form-control" type="email" name="email" id="email" placeholder="Email">
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4 mt-5">
-            <label class="form-label" for="nom">
-                <div class="badge badge-dark text-wrap">Nom</div>
-            </label>
-            <input class="form-control" type="text" name="nom" id="nom" placeholder="Nom">
-        </div>
-
-        <div class="col-md-4 mt-5">
-            <label class="form-label" for="prenom">
-                <div class="badge badge-dark text-wrap">Prénom</div>
-            </label>
-            <input class="form-control" type="text" name="prenom" id="prenom" placeholder="Prénom">
+            <div class="col-md-4 mt-5">
+                <label class="form-label" for="adresse">
+                    <div class="badge badge-dark text-wrap">Adresse</div>
+                </label>
+                <input class="form-control" type="text" name="adresse" id="adresse" placeholder="Adresse">
+            </div>
         </div>
 
-        <div class="col-md-4 mt-4">
-            <p>
-            <div class="badge badge-dark text-wrap">Civilité</div>
-            </p>
-
-            <input type="radio" name="civilite" id="civilite1" value="femme">
-            <label class="mx-2" for="civilite1">Femme</label>
-
-            <input type="radio" name="civilite" id="civilite2" value="homme">
-            <label class="mx-2" for="civilite2">Homme</label>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4 mt-5">
-            <label class="form-label" for="ville">
-                <div class="badge badge-dark text-wrap">Ville</div>
-            </label>
-            <input class="form-control" type="text" name="ville" id="ville" placeholder="Ville">
+        <div class="col-md-1 mt-5">
+            <button type="submit" class="btn btn-outline-dark btn-warning">Valider</button>
         </div>
 
-        <div class="col-md-4 mt-5">
-            <label class="form-label" for="code_postal">
-                <div class="badge badge-dark text-wrap">Code Postal</div>
-            </label>
-            <input class="form-control" type="text" name="code_postal" id="code_postal" placeholder="Code postal">
-        </div>
-
-        <div class="col-md-4 mt-5">
-            <label class="form-label" for="adresse">
-                <div class="badge badge-dark text-wrap">Adresse</div>
-            </label>
-            <input class="form-control" type="text" name="adresse" id="adresse" placeholder="Adresse">
-        </div>
-    </div>
-
-    <div class="col-md-1 mt-5">
-        <button type="submit" class="btn btn-outline-dark btn-warning">Valider</button>
-    </div>
-
-</form>
+    </form>
 <?php endif; ?>
 
-<h2 class="py-5">Nombre de ... en base de données: </h2>
+<!-- requete SQL pour récupérer le nb d'utilisateurs inscrits en BDD, nb que je pourrais afficher grace à rowCount deux lignes en dessous -->
+<?php $nbUsers = $pdo->query("SELECT id_membre FROM membre"); ?>
+<h2 class="py-5">Nombre d'utilisateurs en base de données: <?= $nbUsers->rowCount() ?></h2>
 
 <div class="row justify-content-center py-5">
-    <a href=''>
+    <a href='?action=add'>
         <button type="button" class="btn btn-sm btn-outline-dark shadow rounded">
             <i class="bi bi-plus-circle-fill"></i> Ajouter un utilisateur
         </button>
@@ -209,26 +293,29 @@ require_once('includeAdmin/header.php');
 
 <!-- modal -->
 
-<!-- modal infos -->
-<div class="modal fade" id="myModalUsers" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title text-warning" id="exampleModalLabel">Gestion des utilisateurs</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Gérez ici votre base de données des utilisateurs</p>
-                <p>Vous pouvez modifier leurs données, ajouter ou supprimer un utilisateur</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-outline-warning text-dark" data-dismiss="modal">Fermer</button>
+<!-- pour empecher la modale de s'ouvrir à chaque rafraichissement de page, le temps de terminer de coder cette page -->
+<?php if (isset($_GET['action']) || isset($_GET['page'])) : ?>
+    <!-- modal infos -->
+    <div class="modal fade" id="myModalUsers" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-warning" id="exampleModalLabel">Gestion des utilisateurs</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Gérez ici votre base de données des utilisateurs</p>
+                    <p>Vous pouvez modifier leurs données, ajouter ou supprimer un utilisateur</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-warning text-dark" data-dismiss="modal">Fermer</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!-- modal -->
+    <!-- modal -->
+<?php endif; ?>
 
 <?php require_once('includeAdmin/footer.php'); ?>
